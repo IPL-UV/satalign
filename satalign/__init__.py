13ebf3cec4b7cd094b3cb87c05da9388af46eff3
Julio Cesar Contreras Huerta <126512018+JulioContrerasH@users.noreply.github.com>
2024-08-22 12:32:08 -0500

    soft dependencies

--- satalign/__init__.py
+++ satalign/__init__.py
@@ -1,4 +1,4 @@
 from satalign import utils
-from satalign.ecc import ECC
-from satalign.lgm import LGM
-from satalign.pcc import PCC
+#from satalign.ecc import ECC
+#from satalign.lgm import LGM
+#from satalign.pcc import PCC
